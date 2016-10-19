09e9de00cf22d99ad9210b0a096fae09ca60feba
hangilc <hangil@chang.jp>
2016-10-20 00:02:02 +0900

    temporary saving

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var fs = require("fs");
+var path = require("path");
+var init = require("ini");
 
 function Config(){
 	this.config = {};
@@ -36,11 +38,58 @@ Config.prototype.addDefaults = function(key, obj){
 	});
 };
 
-Config.prototype.readDir = function(dirpath){
-	fs.readdirSync(dirpath).forEach(function(path){
-		console.log(path);
-	});
+var re = /^config-(.+)\.([^.]*)/;
+
+function isConfigFile(filename){
+	var m = filename.match(re);
+	if( m ){
+		return {
+			name: m[1],
+			ext: m[2]
+		};
+	}	
+}
+
+Config.prototype.readJs = function(key, pathname){
+	console.log(pathname);
+	var obj = require(pathname);
+	this.set(key, obj);
 };
 
-module.exports = Config;
+Config.prototype.readIni = function(key, pathname){
+	var src = fs.readFileSync(pathname);
+	console.log(src);
+	throw new Error("not implemented yet");
+};
+
+Config.prototype.readJson = function(key, pathname){
+	var src = fs.readFileSync(pathname);
+	this.set(key, JSON.parse(src));
+};
+
+Config.prototype.readDir = function(dirpath){
+	fs.readdirSync(dirpath).forEach(function(filename){
+		var m = isConfigFile(filename);
+		if( !m ){
+			return;
+		}
+		var key = m.name;
+		var fullpath = path.join(dirpath, filename);
+		switch(m.ext){
+			case "js": this.readJs(key, fullpath); break;
+			case "ini": this.readIni(key, fullpath); break;
+			case "json": this.readJson(key, fullpath); break;
+			default: throw new Error("cannot handle config file: " + fullpath);
+		}
+	}, this);
+};
+
+exports.create = function(){
+	var config = new Config();
+	var configDir = process.env.MYCLINIC_CONFIG_DIR;
+	if( configDir ){
+		config.readDir(configDir);
+	}
+	return config;
+};
 
